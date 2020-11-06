f0436d2853ef451d7c8b4a4e17c3321ccc6709f3
diaslilian <lilian.dias25@gmail.com>
2020-11-05 21:56:10 -0300

    add mongoose

--- src/app.js
+++ src/app.js
@@ -1,24 +1,38 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/clientes", {
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.log.bind(console, "connection error: "));
+db.once("open", () => {
+  console.log("Conexao feita com sucesso");
+});
 
 //rotas
-const clientes = require("./routes/clientesRoute")
+const clientes = require("./routes/clientesRoute");
 
 //configurar body parser
 app.use(bodyParser.json());
-// app.use(express.json()); - Podemos usar a propria função de parser de json do express, sem a necessidade de instalar o body parser 
+// app.use(express.json()); - Podemos usar a propria função de parser de json do express, sem a necessidade de instalar o body parser
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-    )
-    next()
-  })
+  );
+  next();
+});
 
-app.use("/clientes", clientes)
+app.use("/clientes", clientes);
 
-module.exports = app
+module.exports = app;
