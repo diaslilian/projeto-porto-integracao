a93905d6a42bb450a1efef684ef75876d33177f5
diaslilian <lilian.dias25@gmail.com>
2020-11-05 21:56:55 -0300

    add models

---
+++ src/models/clientes.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const clientesSchema = new mongoose.Schema(
+  {
+    nome: { type: String },
+    email: { type: String },
+    cpf: { type: Number },
+    dataNascimento: { type: String },
+    estadoCivil: { type: String },
+    telefone: { type: Number },
+    comprou: { type: Boolean },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const clientes = mongoose.model("clientes", clientesSchema);
+
+module.exports = clientes;
