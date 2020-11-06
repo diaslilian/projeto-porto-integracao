41141164ff0b762e6ada5a929bcfac4351b2d33f
diaslilian <lilian.dias25@gmail.com>
2020-11-05 21:56:41 -0300

    update controllers

--- src/controllers/clientesController.js
+++ src/controllers/clientesController.js
@@ -1,26 +1,52 @@
+const clientes = require("../models/clientes");
+
 const getAll = (req, res) => {
-    console.log("getAll");
-      res.status(200).send("ok");
+  clientes.find((err, clientes) => {
+    if (err) res.status(500).send(err);
+
+    res.status(200).send(clientes);
+  });
 };
 
 const getCompradores = (req, res) => {
-    console.log("getCompradores");
-      res.status(200).send("ok");
+  clientes.find(
+    { comprou: true },
+    { nome: 1, email: 1, _id: 0 },
+    (err, clientes) => {
+      if (err) res.status(500).send(err);
+
+      res.status(200).send(clientes);
+    }
+  );
 };
 
 const getByCpf = (req, res) => {
-    console.log("getByCpf");
-      res.status(200).send("ok");
+  const cpf = req.params.cpf;
+
+  clientes.find({ cpf }, (err, clientes) => {
+    if (err) res.status(500).send(err);
+
+    res.status(200).send(clientes);
+  });
 };
 
 const postCliente = (req, res) => {
-    console.log("postCliente");
-      res.status(200).send("ok");
+  console.log(req.body);
+  const cliente = new clientes(req.body);
+
+  cliente.save((err) => {
+    if (err) res.status(500).send(err);
+
+    res.status(201).send({
+      status: true,
+      mensagem: "Cliente incluido com sucesso",
+    });
+  });
 };
 
 module.exports = {
-    getAll,
-    getCompradores,
-    getByCpf,
-    postCliente
-}
+  getAll,
+  getCompradores,
+  getByCpf,
+  postCliente,
+};
